e0409b4a7fcd01cec5a2ec057d2230e67be7a50d
Stephan <vanaf1979@gmail.com>
2019-03-08 14:57:49 +0100

    Fixed paths after theme move

--- components/writing.php
+++ components/writing.php
@@ -1,85 +1,85 @@
 
 <section class="writingheader row">
 			
-            <div class="item pure-g row-center">
-        
-                <div class="image pure-u-1 pure-u-sm-1 pure-u-md-1pure-u-lg-1 pure-u-xl-1">
-        
-                    <h2>Writing</h2>
-                    <p>A selection of my WordPress writing</p>
-        
-                </div>
-        
-            </div>
-        
-        </section>
-        
-        <section class="writing row-center">
-                    
-            <div class="item pure-g row-center">
-        
-                <div class="image pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
-                
-                    <img src="/wp-content/themes/vanaf1979/public/img/wordpress-dev-monthly-recap-february-socials.jpg" alt="" />
-        
-                </div>
-                
-                <div class="text pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
+    <div class="item pure-g row-center">
+
+        <div class="image pure-u-1 pure-u-sm-1 pure-u-md-1pure-u-lg-1 pure-u-xl-1">
+
+            <h2>Writing</h2>
+            <p>A selection of my WordPress writing</p>
+
+        </div>
+
+    </div>
+
+</section>
+
+<section class="writing row-center">
             
-                    <article>
-                        
-                        <h4>Wordpress Dev Monthly Recap for January.</h4>
-                        <p>A monthly recap of wordpress (and related) articles, tutorials and resources for January 2019</p>
-                        <a href="./item.html">Read article</a>
-                        
-                    </article>
+    <div class="item pure-g row-center">
+
+        <div class="image pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
         
-                </div>
+            <img src="/wp-content/themes/vanaf1979/public/img/wordpress-dev-monthly-recap-february-socials.jpg" alt="" />
+
+        </div>
+        
+        <div class="text pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
+    
+            <article>
                 
-            </div>
-            
-            <div class="item pure-g row-center">
-        
-                <div class="image pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
+                <h4>Wordpress Dev Monthly Recap for January.</h4>
+                <p>A monthly recap of wordpress (and related) articles, tutorials and resources for January 2019</p>
+                <a href="./item.html">Read article</a>
                 
-                    <img src="/wp-content/themes/vanaf1979/public/img/wordpress-dev-monthly-recap-february-socials.jpg" alt="" />
+            </article>
+
+        </div>
         
-                </div>
+    </div>
+    
+    <div class="item pure-g row-center">
+
+        <div class="image pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
+        
+            <img src="/wp-content/themes/vanaf1979/public/img/wordpress-dev-monthly-recap-february-socials.jpg" alt="" />
+
+        </div>
+        
+        <div class="text pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
+    
+            <article>
                 
-                <div class="text pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
-            
-                    <article>
-                        
-                        <h4>Multiple Custom Templates for Posts and Custom Post Types.</h4>
-                        <p>A monthly recap of wordpress (and related) articles, tutorials and resources for January 2019</p>
-                        <a href="./item.html">Read article</a>
-                        
-                    </article>
-        
-                </div>
+                <h4>Multiple Custom Templates for Posts and Custom Post Types.</h4>
+                <p>A monthly recap of wordpress (and related) articles, tutorials and resources for January 2019</p>
+                <a href="./item.html">Read article</a>
                 
-            </div>
+            </article>
+
+        </div>
         
-            <div class="item pure-g row-center">
+    </div>
+
+    <div class="item pure-g row-center">
+
+        <div class="image pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
         
-                <div class="image pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
+            <img src="/wp-content/themes/vanaf1979/public/img/wordpress-dev-monthly-recap-february-socials.jpg" alt="" />
+
+        </div>
+        
+        <div class="text pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
+    
+            <article>
                 
-                    <img src="/wp-content/themes/vanaf1979/public/img/wordpress-dev-monthly-recap-february-socials.jpg" alt="" />
-        
-                </div>
+                <h4>Wordpress Dev Monthly Recap for December.</h4>
+                <p>A monthly recap of wordpress (and related) articles, tutorials and resources for January 2019</p>
+                <a href="./item.html">Read article</a>
                 
-                <div class="text pure-u-1 pure-u-sm-1-2 pure-u-md-1-2 pure-u-lg-1-2 pure-u-xl-2">
-            
-                    <article>
-                        
-                        <h4>Wordpress Dev Monthly Recap for December.</h4>
-                        <p>A monthly recap of wordpress (and related) articles, tutorials and resources for January 2019</p>
-                        <a href="./item.html">Read article</a>
-                        
-                    </article>
+            </article>
+
+        </div>
         
-                </div>
-                
-            </div>
-            
-        </section>	
+    </div>
+    
+</section>	
