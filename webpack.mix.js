9fb55ca94d0c5767794b7d341bc5f1993dd5e146
Stephan <vanaf1979@gmail.com>
2019-03-08 14:32:08 +0100

    Updated webpack.mix.js and package.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,9 @@
 const mix = require('laravel-mix');
 
 mix.js( 'src/js/app.js', 'public/js/app.js').extract()
-.sass( 'src/scss/app.scss', 'public/css/app.css')
+.sass( 'src/scss/header.scss', 'public/css/header.css')
+.sass( 'src/scss/footer.scss', 'public/css/footer.css')
 .options({
     processCssUrls: false
 })
-.webpackConfig({
-    plugins: [
-        new CopyWebpackPlugin([
-            {
-                from:  themePath + 'src/img',
-                to:  themePath + 'public/img',
-            }
-        ]),
-        new ImageminPlugin({
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            plugins: [
-                imageminMozjpeg({
-                    quality: 80,
-                })
-            ]
-        })
-    ]
-});
+.copyDirectory('src/img', 'public/img');
